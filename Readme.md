2fdd4f32492c956251c9ff362719cb97577f4ef9
slamhan <slamhan1987@hotmail.com>
2018-05-03 14:50:51 +0800

    add docs.

--- Readme.md
+++ Readme.md
@@ -8,3 +8,35 @@
  ###run app by using sbt
     
     sbt run .
+    
+###prepare init data
+
+```
+RUN demo-application/test/endpoint/OAuthEndPointDataInit.scala
+```
+ 
+Try to create access tokens using curl
+
+### Client credentials
+
+```
+$ curl http://localhost:9000/oauth/access_token -X POST -d "client_id=oauth2-client-id" -d "client_secret=oauth2-client-secret" -d "grant_type=client_credentials"
+```
+   
+```
+{"token_type":"Bearer","access_token":"h8IKJfKhx47ZDf25akbTRKpHgV7QJFOFEpQcJqsv","expires_in":3600,"refresh_token":"g8OBI0IMXKleE99R9JxWg3JGanq7uPGzuDQ9Chnt"}
+```
+
+### Refresh token
+
+```
+$ curl http://localhost:9000/oauth/access_token -X POST -d "client_id=oauth2-client-id" -d "client_secret=oauth2-client-secret" -d "refresh_token=EuMLdSVUJqhMOhVlb6xeLlNmf4gfuDMMgDqr7P1l" -d "grant_type=refresh_token"
+```
+
+### Access resource using access_token
+
+You can access application resource using access token
+
+```
+$ curl --dump-header - -H "Authorization: Bearer ${access_token}" http://localhost:9000/resources
+```
---
+++ demo-application/test/endpoint/OAuthEndPointDataInit.scala
@@ -0,0 +1,155 @@
+package endpoint
+
+import akka.actor.ActorSystem
+import com.jayway.restassured.path.json.JsonPath
+import com.redis.RedisClientPool
+import com.typesafe.config.ConfigFactory
+import org.hrw.login.service.mongodb.{Account, AccountDAO, MongoDB}
+import org.hrw.login.service.oauth2
+import org.hrw.login.service.oauth2.{OauthAccessToken, OauthClient}
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FlatSpec, MustMatchers}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+import scala.collection.JavaConverters._
+
+
+class OAuthEndPointSpec extends FlatSpec with MockitoSugar with MustMatchers with BeforeAndAfter {
+  implicit var accountDAO: AccountDAO = null
+  val actorSystem: ActorSystem = ActorSystem.create("test")
+
+  val clientId: String = "oauth2-client-id"
+  val clientSecret: String = "oauth2-client-secret"
+  val grantType: String = "client_credentials"
+  //  val redisServer: RedisServer = RedisServer.builder().port(6378).build()
+  implicit var redisPool: RedisClientPool = null
+  // by default port = 12345 & version = Version.2.3.0
+  // add your own port & version parameters in mongoStart method if you need it
+
+  var mongoDB: MongoDB = null
+
+  var oAuthEndPoint: OAuthEndPoint = null
+
+  before {
+    mongoDB = new MongoDB(ConfigFactory.load("application.test.conf"))
+    accountDAO = new AccountDAO(mongoDB)
+    redisPool = new RedisClientPool("localhost", 6379)
+    oAuthEndPoint = new OAuthEndPoint(actorSystem)
+  }
+
+
+  "OAuthEndPoint" should "return 400 without token in header" in {
+    val request = FakeRequest()
+    val result = oAuthEndPoint.test(request)
+    status(result) must be(400)
+  }
+
+  "OAuthEndPoint" should "return 401 with invalid token" in {
+    val request = FakeRequest().withHeaders("Authorization" -> "Bearer ${accessToken.accessToken}")
+    val result = oAuthEndPoint.test(request)
+    status(result) must be(401)
+  }
+
+  "OAuthEndPoint" should "return user info success" in {
+    val account = oauth2.Account(1L, "password", "oauth2@demo.com")
+    //Create ${clientId}#${clientSecret}
+    val oauthClient = OauthClient(
+      id = 0L,
+      ownerId = 1L,
+      owner = Some(account),
+      grantType = grantType,
+      clientId = clientId,
+      clientSecret = clientSecret,
+      redirectUri = None,
+      createdAt = 0L)
+    OauthClient.create(oauthClient)
+    //Create user
+    accountDAO.save(Account(userName = "oauth2", password = "password", email = "oauth2@demo.com"))
+    //Create oauth access token
+    val accessToken = OauthAccessToken.create(account, oauthClient)
+    val request = FakeRequest().withHeaders("Authorization" -> s"Bearer ${accessToken.accessToken}")
+    val result = oAuthEndPoint.test(request)
+    status(result) must be(200)
+    new JsonObject(contentAsString(result)).getString("content.email") must be("oauth2@demo.com")
+  }
+
+}
+
+class JsonObject(in: String) {
+  val json = in
+  val ctx = new JsonPath(json)
+
+  def get[T](path: String): T = {
+    ctx.get[T](path)
+  }
+
+  def get[T](): T = {
+    ctx.get()
+  }
+
+  def getBoolean(path: String): Boolean = {
+    ctx.getBoolean(path)
+  }
+
+  def getByte(path: String): Byte = {
+    ctx.getByte(path)
+  }
+
+  def getChar(path: String): Char = {
+    ctx.getChar(path)
+  }
+
+  def getDouble(path: String): Double = {
+    ctx.getDouble(path)
+  }
+
+  def getFloat(path: String): Float = {
+    ctx.getFloat(path)
+  }
+
+  def getInt(path: String): Int = {
+    ctx.getInt(path)
+  }
+
+  def getJsonObject[T](path: String): T = {
+    ctx.getJsonObject[T](path)
+  }
+
+  def getList[T](path: String): List[T] = {
+    ctx.getList[T](path).asScala.toList
+  }
+
+  def getList[T](path: String, clazz: Class[T]): List[T] = {
+    ctx.getList[T](path, clazz).asScala.toList
+  }
+
+  def getLong(path: String): Long = {
+    ctx.getLong(path)
+  }
+
+  def getMap[K, V](path: String): Map[K, V] = {
+    ctx.getMap[K, V](path).asScala.toMap
+  }
+
+  def getMap[K, V](path: String, keyType: Class[K], valueType: Class[V]): Map[K, V] = {
+    ctx.getMap[K, V](path, keyType, valueType).asScala.toMap
+  }
+
+  def getObject[T](path: String, clazz: Class[T]): T = {
+    ctx.getObject[T](path, clazz)
+  }
+
+  def getShort(path: String): Short = {
+    ctx.getShort(path)
+  }
+
+  def getString(path: String): String = {
+    ctx.getString(path)
+  }
+
+
+  override def toString = {
+    json
+  }
+}
